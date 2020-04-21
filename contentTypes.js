2c16142ea6f512d5b67f20439f653276559b80e4
mbareeva <beeemaja@gmail.com>
2020-04-21 21:07:05 +0200

    contentType.js for main/router js. Images are added.

---
+++ contentTypes.js
@@ -0,0 +1,17 @@
+module.exports = {
+    html: {
+        "Content-Type": "text/html"
+    },
+    css: {
+        "Content-Type": "text/css"
+    },
+    png: {
+        "Content-Type": "image/png"
+    },
+    jpg: {
+        "Content-Type": "image/jpg"
+    },
+    js: {
+        "Content-Type": "text/js"
+    },
+}
