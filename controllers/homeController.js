09cc2fbab30678130e99842a98f3e51be83800d9
sophieMitPh <sophie.pr@gmx.de>
2020-04-28 16:02:36 +0200

    setup express

---
+++ controllers/homeController.js
@@ -0,0 +1,8 @@
+exports.getProductOverview = (req, res) => {
+    res.render("productOverview");
+};
+
+exports.getProductDetailView = (req, res) => {
+    res.render("productDetailView");
+};
+
--- main.js
+++ main.js
@@ -1,53 +1,23 @@
 const port = 3000,
-    http = require("http"),
-    httpStatusCodes = require("http-status-codes"),
-    router = require("./router"),
-    contentTypes = require("./contentTypes"),
-    utils = require("./utils");
+    express = require('express'),
+    app = express();
 
-router.get("/", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.plaintxt);
-    res.end("Got to /productOverview.html or /productDetailView.html for more");
+const homeController = require('./controllers/homeController');
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.send("Go to /productOverview.ejs or /productDetailView.ejs for more");
 });
-router.get("/productOverview.html", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.html);
-    utils.getFile("views/productOverview.html", res);
+
+app.use((req, res, next) => {
+    console.log(`request made to: ${req.url}`);
+    next();
 });
-router.get("/productDetailView.html", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.html);
-    utils.getFile("views/productDetailView.html", res);
+
+app.listen(port, () => {
+    console.log(`The express server has started on port ${port}`);
 });
-router.get("/cart.png", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.png);
-    utils.getFile("public/images/cart.png", res)
-});
-router.get("/logo.png", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.png);
-    utils.getFile("public/images/logo.png", res)
-});
-router.get("/account.png", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.png);
-    utils.getFile("public/images/account.png", res)
-});
-router.get("/nopicture.png", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.png);
-    utils.getFile("public/images/nopicture.png", res)
-});
-router.get("/wishlist.png", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.png);
-    utils.getFile("public/images/wishlist.png", res)
-});
-router.get("/header.css", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.css);
-    utils.getFile("public/css/header.css", res)
-});
-router.get("/products.css", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.css);
-    utils.getFile("public/css/products.css", res)
-});
-router.get("/product.css", (req, res) => {
-    res.writeHead(httpStatusCodes.OK, contentTypes.css);
-    utils.getFile("public/css/product.css", res)
-});
-http.createServer(router.handle).listen(3000);
-console.log(`The server is listening on port number: ${port}`);
+
+app.get("/productOverview.ejs", homeController.getProductOverview);
+app.get("/productDetailView.ejs", homeController.getProductDetailView);
