47d2a66a35fee72abec5c6a89e58d6db54c1eee3
sophieMitPh <sophie.pr@gmx.de>
2020-05-01 13:06:12 +0200

    fixed not found error // changed order in main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const port = 3000,
     app = express();
 
 const homeController = require('./controllers/homeController');
+const errorController = require('./controllers/errorController');
 const layouts = require("express-ejs-layouts");
 
 app.set("view engine", "ejs");
@@ -12,16 +13,18 @@ app.use(express.static('public'));
 app.get("/", (req, res) => {
     res.send("Go to /product/:product or /products for more");
 });
+app.get("/products", homeController.getProductOverview);
+app.get("/product/:product", homeController.getProductDetailView);
 
 app.use((req, res, next) => {
     console.log(`request made to: ${req.url}`);
     next();
 });
+app.use(errorController.logErrors);
+app.use(errorController.respondInternalError);
+app.use(errorController.respondNoResourceFound);
 
 app.listen(port, () => {
     console.log(`The express server has started on port ${port}`);
 });
-
-app.get("/products", homeController.getProductOverview);
-app.get("/product/:product", homeController.getProductDetailView);
 
