70e4684827f07f830a9cf7d32a331557a0ebc8f0
sophieMitPh <sophie.pr@gmx.de>
2020-05-01 12:34:42 +0200

    added error handling

--- main.js
+++ main.js
@@ -4,14 +4,21 @@ const port = 3000,
 
 const homeController = require('./controllers/homeController');
 const layouts = require("express-ejs-layouts");
+const errorController = require('./controllers/errorController');
 
 app.set("view engine", "ejs");
 app.use(layouts);
 app.use(express.static('public'));
 
+app.use(errorController.logErrors);
+app.use(errorController.respondInternalError);
+app.use(errorController.respondNoResourceFound);
+
 app.get("/", (req, res) => {
     res.send("Go to /product/:product or /products for more");
 });
+app.get("/products", homeController.getProductOverview);
+app.get("/product/:product", homeController.getProductDetailView);
 
 app.use((req, res, next) => {
     console.log(`request made to: ${req.url}`);
@@ -21,7 +28,3 @@ app.use((req, res, next) => {
 app.listen(port, () => {
     console.log(`The express server has started on port ${port}`);
 });
-
-app.get("/products", homeController.getProductOverview);
-app.get("/product/:product", homeController.getProductDetailView);
-
