baf557808bab66d9ee27c52ba81cb3d485babef6
mbareeva <beeemaja@gmail.com>
2020-05-23 11:44:26 +0200

    fixed bug in referencing in subscriber.js

--- models/subscriber.js
+++ models/subscriber.js
@@ -21,7 +21,7 @@ const mongoose = require('mongoose'),
 		//every subscriber can add the product to the card even not being signed in.
 		products: [
 			{
-			type: Schema.Types.ObjectId,
+			type: mongoose.Schema.Types.ObjectId,
 			 ref: "Product"
 			}
 		]	
@@ -30,7 +30,10 @@ const mongoose = require('mongoose'),
 		timeStamps: true
 	}
 );
-
+subscriberSchema.virtual('fullName')
+	.get(function() {
+		return `${this.name} ${this.lastname}`;
+	});
 subscriberSchema.methods.getInfo = function() {
 	return `Name: ${this.name} Email: ${this.email}`;
 };
