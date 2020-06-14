3385531de2a8c22c93faba9cc73566c6defa1af2
mbareeva <beeemaja@gmail.com>
2020-06-14 13:31:50 +0200

    fixed the route for detailed product view and href in view

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,14 +4,13 @@ const router = require('express').Router(),
 router.get('/', productController.index, productController.indexView);
 router.get('/productOverview', productController.getAllProducts);
 router.get('/new', productController.new);
-router.get('/:name', productController.getDetailedView);
-
-router.get('/:id', productController.show, productController.showView);
 router.post('/create',
 	productController.create,
 	productController.redirectView);
 router.get('/:id/edit', productController.edit);
 router.put('/:id/update', productController.update, productController.redirectView);
+router.get('/:id', productController.show, productController.showView);
+router.get('/product/:name', productController.getDetailedView);
 router.delete( '/:id/delete',
 	productController.delete,
 	productController.redirectView
